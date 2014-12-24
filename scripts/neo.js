ba48cb0b27019237d970da1bbbb2ae485aee0d8f
jdbranham <Jeremy.D.Branham@Sprint.com>
2014-12-24 11:30:50 -0600

    This version is compatible with Neo4J 1.x

--- scripts/neo.js
+++ scripts/neo.js
@@ -1,48 +1,62 @@
 function Neo(urlSource) {
 	function txUrl() {
 		var url = (urlSource() || "http://localhost:7474").replace(/\/db\/data.*/,"");
-		return url + "/db/data/transaction/commit";
+		return url + "/db/data/cypher";
 	}
 	var me = {
 		executeQuery: function(query, params, cb) {
 			$.ajax(txUrl(), {
 				type: "POST",
 				data: JSON.stringify({
-					statements: [{
-						statement: query,
-						parameters: params || {},
-						resultDataContents: ["row", "graph"]
-					}]
+					  query : query,
+					  params : params || {}
 				}),
 				contentType: "application/json",
 				error: function(err) {
 					cb(err);
 				},
 				success: function(res) {
-					if (res.errors.length > 0) {
-						cb(res.errors);
+					if (res.exception) {
+						cb(res.exception);
 					} else {
-						var cols = res.results[0].columns;
-						var rows = res.results[0].data.map(function(row) {
+						var cols = res.columns;
+						var rows = res.data.map(function(row) {
 							var r = {};
 							cols.forEach(function(col, index) {
-								r[col] = row.row[index];
+								r[col] = row[index];
 							});
 							return r;
 						});
 						var nodes = [];
 						var rels = [];
 						var labels = [];
-						res.results[0].data.forEach(function(row) {
-							row.graph.nodes.forEach(function(n) {
+						res.data.forEach(function(row) {
+							
+							var n = row[0]['data'];
+							
+							for (var property in n) {
+							    if (Object.hasOwnProperty(property)) {
+							    	var found = nodes.filter(function (m) { return m.id == n.id; }).length > 0;
+									   if (!found) {
+										  var node = n.properties||{}; node.id=n.id;node.type=n.name;
+										  nodes.push(node);
+										  if (labels.indexOf(node.type) == -1) labels.push(node.type);
+									   }
+							    }
+							}
+							
+							//rels = rels.concat(row.graph.relationships.map(function(r) { return { source:r.startNode, target:r.endNode, caption:r.type} }));
+							
+							/*row.graph.nodes.forEach(function(n) {
 							   var found = nodes.filter(function (m) { return m.id == n.id; }).length > 0;
 							   if (!found) {
 								  var node = n.properties||{}; node.id=n.id;node.type=n.labels[0];
 								  nodes.push(node);
 								  if (labels.indexOf(node.type) == -1) labels.push(node.type);
 							   }
-							});
-							rels = rels.concat(row.graph.relationships.map(function(r) { return { source:r.startNode, target:r.endNode, caption:r.type} }));
+							});*/
+							
+							//rels = rels.concat(row.graph.relationships.map(function(r) { return { source:r.startNode, target:r.endNode, caption:r.type} }));
 						});
 						cb(null,{table:rows,graph:{nodes:nodes, edges:rels},labels:labels});
 					}
